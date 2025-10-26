7bdd060ba2e5be8cb5b91db2efcad7f117da0354
Daniil345689 <danya060806@gmail.com>
2025-10-26 19:17:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,113 @@
+const $btn = document.getElementById('btn-kick');
+/*const $mybtn = document.getElementById('btn-kick1');
+const $myBtn = document.getElementById('btn-kick1');*/
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+    renderProgressbarHP: renderProgressbarHP,
+     renderHPLife: renderHPLife,
+     renderHP: renderHP,
+     startHP: startHP,
+    changeHP:changeHP,
+   //  mychangeHP: mychangeHP,
+}
+
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+    renderProgressbarHP: renderProgressbarHP,
+     renderHPLife: renderHPLife,
+     renderHP: renderHP,
+     startHP: startHP,
+    changeHP:changeHP,
+   //  mychangeHP: mychangeHP,
+}
+
+$btn.addEventListener('click', function () {
+    console.log('Kick');
+    character.changeHP(random(20));
+    enemy.changeHP(random(20));
+});
+
+function init() {
+    console.log('Start Game!');
+    character.renderHP();
+    enemy.renderHP();
+}
+
+function startHP() {
+    this.damageHP = 100;
+}
+
+function renderHP() {
+    this.renderHPLife();
+    this.renderProgressbarHP();
+}
+
+function renderHPLife() {
+    this.elHP.innerText = this.damageHP + ' / '+ this.defaultHP;
+}
+
+function renderProgressbarHP() {
+    this.elProgressbar.style.width = this.damageHP + '%';
+}
+
+function changeHP(count) {
+    if (this.damageHP < count) {
+        this.damageHP = 0;
+        alert('Бедный '+ this.name +' проиграл бой!');
+        $btn.disabled = true;
+    testDamageHP (character, enemy);
+    }
+    else{    
+    this.damageHP -= count;
+    }
+    this.renderHP();
+}
+
+function testDamageHP (person1,person2) {
+    if (person1.damageHP === 0 || person2.damageHP === 0) {
+        enemy.startHP();
+        character.startHP();
+    }
+}
+
+/*function mychangeHP() {
+    if (this.damageHP < count) {
+        this.damageHP = 0;
+        alert('Бедный '+ this.name +' проиграл бой!')
+    }
+    this.damageHP -= count;
+
+    renderHP();
+}*/
+
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
+
+function generateLog() {
+   const logs = [
+    '${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name},не помня себя от испуга, ударил в предплечье врага.',
+    '${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага.',
+    '${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил.',
+    '${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name}случайно нанес мощнейший удар.',
+    '${firstPerson.name} поперхнулся, но в это время ${secondPerson.name}нехотя раздробил кулаком \<вырезанно цензурой\> противника.',
+   '${firstPerson.name} удивился, а ${secondPerson.name}пошатнувшись влепил подлый удар.',
+    '${firstPerson.name} высморкался, но неожиданно ${secondPerson.name}пошатнувшись влепил подлый удар.',
+    '${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника.',
+    '${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника.',
+    '${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику.',
+];
+         
+return logs[random(logs.length) - 1]
+
+}
+
+init();
